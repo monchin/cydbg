92f84c555d9f4d39cef4e30b60df3643fb8a6e5c
c.men <c.men@polarisbiology.com>
2024-05-22 15:15:10 +0800

    add asan for debug

--- setup.py
+++ setup.py
@@ -19,14 +19,15 @@ srcs = [
 include_dirs = [str(PRJ_PATH / "cpp" / "include")]
 library_dirs = []
 libraries = []
+extra_link_args = []
 
 PFSYS = platform.system()
 if PFSYS == "Windows":
     extra_compile_args = ["/std:c++17"]
-    extra_link_args = []
 elif PFSYS == "Linux":
-    extra_compile_args = ["-std=c++17",]
-    extra_link_args = []
+    extra_compile_args = [
+        "-std=c++17",
+    ]
 else:
     raise NotImplementedError("Only support Windows and Linux")
 
@@ -34,18 +35,24 @@ else:
 if __name__ == "__main__":
     define_macros = []
 
-    # 是否debug模式
+    # debug or not
     if_debug = input("compile by DEBUG? y/(n):").lower()
     while if_debug not in DEBUG_DICT.keys():
         if_debug = input("wrong input, please type again: y/(n):").lower()
     need_debug = DEBUG_DICT[if_debug]
     if need_debug is True:
         if PFSYS == "Windows":
-            extra_compile_args += ["/Zi", "/Od"]
+            extra_compile_args += ["/Zi", "/Od", "/fsanitize=address"]
             extra_link_args += ["/DEBUG"]
         else:
-            extra_compile_args += ["-g", "-O0"]
-        
+            extra_compile_args += [
+                "-g",
+                "-fsanitize=address",
+                "-fno-omit-frame-pointer",
+                "-O0",
+            ]
+            extra_link_args += ["-fsanitize=address", "-fno-omit-frame-pointer"]
+
     extension = Extension(
         PROJ_NAME,
         sources=srcs,
@@ -71,5 +78,5 @@ if __name__ == "__main__":
             for each in each_lib_dir.glob(f"*{ext}"):
                 shutil.move(each, save_dir / f"{each.name}")
             if need_debug:
-                for each in each_lib_dir.glob(f"*.pdb"):
+                for each in each_lib_dir.glob("*.pdb"):
                     shutil.move(each, save_dir / f"{each.name}")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
+import multiprocessing as mp
+
 import numpy as np
 from cydbg.cython import cydbg_cy
 
-if __name__ == '__main__':
-    test_arr = np.zeros((5, ), dtype=np.float32)
-    print("a")
-    cydbg_cy.py_write_in_wrong_loc(test_arr)
-    print("b")
+
+def bad_kernel():
     pyclass = cydbg_cy.PyClass()
     # pyclass.double_free()
-    print("c")
     pyclass.use_after_free()
-    print("d")
+
+
+if __name__ == "__main__":
+    test_arr = np.zeros((5,), dtype=np.float32)
+    p = mp.Process(target=bad_kernel, args=())
+    p.start()
+    cydbg_cy.py_write_in_wrong_loc(test_arr)
+    p.join()
